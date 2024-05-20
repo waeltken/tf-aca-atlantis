b252a0dfe2f0b3ab921cf97cd411098f69e3f373
Clemens Wältken <clemens@waeltken.de>
2024-05-20 10:40:03 +0200

    Type all variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,33 @@
 variable "location" {
   default = "germanywestcentral"
+  type    = string
 }
 
 variable "resource_group_name" {
   default = "atlantis-rg"
+  type    = string
 }
 
 variable "create_resource_group" {
   default = true
+  type    = bool
 }
 
 variable "gh_app_id" {
+  type = string
 }
 
 variable "gh_app_key" {
-
+  type = string
 }
 variable "gh_webhook_secret" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "repo_whitelist" {
+  type = string
 }
